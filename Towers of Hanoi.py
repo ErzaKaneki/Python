c504c0befdd707a9eef097f66c8226c551184ab6
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-03 15:56:00 -0500

    instaciated 3 stacks left, middle, and right.  stored them in "stacks" list. Asked player for 3+ "plates" with input()

--- Towers of Hanoi.py
+++ Towers of Hanoi.py
@@ -1,1 +1,17 @@
 from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+stacks = []
+
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
+
+num_disks = input("How many disks do you want to play with?")
+while int(num_disks) < 3:
+    num_disks = input("Enter a number greater than or equal to 3\n")
