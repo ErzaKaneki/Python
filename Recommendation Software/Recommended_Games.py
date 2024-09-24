93d761b0d9941af4574497eb6c144fb0607fb7a8
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-24 10:35:16 -0500

    Implemented: recommend() Edited: Game_dict.py made pricing easier to understand. Formated: greeting() search_tool() good_bye() For veiwing ease Tested: greeting() search_tool() good_bye() recommend()

--- Recommendation Software/Recommended_Games.py
+++ Recommendation Software/Recommended_Games.py
@@ -1,10 +1,12 @@
 from Game_dict import game_genres
+import os
+import time
 
 def genre_list(dict):
     sortedkeys = sorted(dict.keys(), key = lambda x:x.lower())
     for i in sortedkeys:
         values = dict[i]
-        print(i)
+        print(i.title())
 
 
 
@@ -25,15 +27,20 @@ def greeting():
 ⠀⠀⠀⠀⠀⢸⣯⡄⢨⣿⣿⣿⣍⢙⣽⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
     print("Thank you for choosing Game Recommendations from Erza!")
-    print("Lets get started on a grabbing some game ideas for you, and to be clear MOST games can be found on steam.")
+    print("Lets get started on a grabbing some game ideas for you, and to be clear MOST games can be found on https://store.steampowered.com/.")
     print("But there could be console only, or even out dated and hard to find game recomendations.")
     print("We look for the 'Best' but our values still might differ from others opinions.")
     print("Here we go!!!")
+    input("\n\nPlease Press any Key to Continue....")
     
 def search_tool(dict):
-    print("What style of game are you looking for?")
-    genre_list(game_genres)
-    search = input("Please make a selection from the list. -> ")
+    print("\n\nWhat style of game are you looking for?\n")
+    genre_list(dict)
+    search = input("\nPlease make a selection from the list. -> ")
+    while search.lower() not in dict:
+        os.system('cls')
+        genre_list(dict)
+        search = input("Please check the list and enter your selection: - > ")
     if search.lower() in dict:
         counter = 1
         while counter <= 5:
@@ -43,5 +50,27 @@ def search_tool(dict):
                     print("Title: " + i.name)
                     print("Price: $" + str(i.price))
                     counter += 1
+    again = input("Would you like to search again? Enter y/n: -> ")
+    while again.lower() != "y" and again.lower() != "n":
+        again = input("Please enter y/n: -> ")
+    if again == "y":
+        os.system("cls")
+        search_tool(dict)
+    else:
+        return
+    
+def good_bye():
+    print("\n\nThank you for using our search, enjoy your selections!\n\n")
+    print("""  __                            _ _ 
+ / _|                          | | |
+| |_ __ _ _ __ _____      _____| | |
+|  _/ _` | '__/ _ \\ \\ /\\ / / _ \\ | |
+| || (_| | | |  __/\\ V  V /  __/ | |
+|_| \\__,_|_|  \\___| \\_/\\_/ \\___|_|_|""")
+    
+def recommend():
+    greeting()
+    search_tool(game_genres)
+    good_bye()
 
-search_tool(game_genres)                
+recommend()
