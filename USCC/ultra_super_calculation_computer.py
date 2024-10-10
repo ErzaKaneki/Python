c892fcb94a1886fbe2fbf85d46d6011ec2124789
Erza Kaneki <nevin.kadlec@gmail.com>
2024-10-10 08:54:56 -0500

    Tested: ultra_super_calculation_computer.py - Failed Debugged: ultra_super_calculation_computer.py Re-Tested: ultra_super_calculation_computer.py - Passed

--- USCC/ultra_super_calculation_computer.py
+++ USCC/ultra_super_calculation_computer.py
@@ -6,7 +6,7 @@ class UltraSuperCalculator:
         self.numbers_index = 1
         self.history_index = 0
         self.temp_history_index = 0
-        self.user_display = ""
+        self.user_display = ("")
         self.update_display(f"{self.name}'s Calculator")
 
     def update_display(self, to_update):
@@ -18,26 +18,27 @@ class UltraSuperCalculator:
     def store_value_to_register(self, value_to_store):
         if self.numbers_index > 21:
             self.numbers_index = 1
-            self.number_registers[self.numbers_index] = int(value_to_store, 2)
-            print(f"{int(value_to_store, 2)} was stored in register address {self.number_registers[self.numbers_index]}")
-            self.numbers_index +=1
+        self.number_registers[self.numbers_index] = int(value_to_store, 2)
+        print(f"{int(value_to_store, 2)} was stored in register address {self.numbers_index}.")
+        self.numbers_index +=1
 
     def load_value_from_register(self, register_address):
         index = int(register_address, 2)
-        int_value = int(self.number_registers[self.numbers_index], 2)
+        int_value = int(self.number_registers[index])
         return int_value
     
     def store_to_history_register(self, result_to_store):
         if self.history_index > 9:
             self.history_index = 0
-            self.history_registers[self.history_index] = int(result_to_store, 2)
-            self.history_index += 1
-            self.temp_history_index = self.history_index
+
+        self.history_registers[self.history_index] = bin(result_to_store)
+        self.history_index += 1
+        self.temp_history_index = self.history_index
 
     def add(self, address_num1, address_num2):
         num1 = self.load_value_from_register(address_num1)
         num2 = self.load_value_from_register(address_num2)
-        calculated_value = sum(num1, num2)
+        calculated_value = num1 + num2
         return calculated_value
     
     def multiply(self, address_num1, address_num2):
@@ -64,7 +65,7 @@ class UltraSuperCalculator:
     
     def get_last_calculation(self):
         self.temp_history_index -= 1
-        last_value = "Last recorded result = " + int(self.history_registers[self.temp_history_index], 2)
+        last_value = f"Last recorded result: {int(self.history_registers[self.temp_history_index], 2)}"
         self.update_display(last_value)
 
     def binary_reader(self, instruction):
@@ -72,13 +73,13 @@ class UltraSuperCalculator:
             print("Invalid Instruction, goodbye.")
             return
         
-        opcode = instruction[0, 5]
-        source_one = instruction[6, 10]
-        source_two = [11, 15]
-        store = instruction[16, 25]
-        function_code = instruction[26, 31]
+        opcode = instruction[:6]
+        source_one = instruction[6:11]
+        source_two = instruction[11:16]
+        store = instruction[16:26]
+        function_code = instruction[26:]
 
-        if opcode == '000001':
+        if opcode == "000001":
             self.store_value_to_register(store)
             return
         elif opcode == '100001':
@@ -91,23 +92,32 @@ class UltraSuperCalculator:
         result = 0
 
         if function_code == '100000':
-            self.update_display(self.add(source_one, source_two))
-            return
+            result = self.add(source_one, source_two)
         elif function_code == '100010':
-            self.update_display(self.subtract(source_one, source_two))
-            return
+            result = self.subtract(source_one, source_two)
         elif function_code == '011000':
-            self.update_display(self.multiply(source_one, source_two))
-            return
+            result = self.multiply(source_one, source_two)
         elif function_code == '011010':
-            self.update_display(self.divide(source_one, source_two))
+            result = self.divide(source_one, source_two)
+        else:
+            self.update_display("Invalid FUNCTION CODE")
             return
-        elif function_code != '000000':
-            self.update_display("Invalid OPCODE")
-            return
-
-# erza = UltraSuperCalculator("Erza")
-# erza.binary_reader("1234567812345678123456781234567") Testing if reader is working
-# erza.binary_reader("12345678123456781234567812345678")
-
         
+        self.store_to_history_register(result)
+        self.update_display(f"The result is: {result}")
+
+erza = UltraSuperCalculator("Erza")
+# Adds 5 and 10 to number registers
+erza.binary_reader("00000100000000000000000101000000")
+erza.binary_reader("00000100000000000000001010000000")
+
+# Adds/Subtracts/Multiplies/Divides 5 and 10 from registers
+erza.binary_reader("00000000001000100000000000100000")
+erza.binary_reader("00000000001000100000000000100010")
+erza.binary_reader("00000000001000100000000000011000")
+erza.binary_reader("00000000001000100000000000011010")
+
+# Gets the last three calculations
+erza.binary_reader("10000100000000000000000000000000")
+erza.binary_reader("10000100000000000000000000000000")
+erza.binary_reader("10000100000000000000000000000000")
