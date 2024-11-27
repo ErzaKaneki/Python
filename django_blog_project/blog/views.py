40de3bd103c8bfe9d44ee51d67fc9f58e3ff197a
Erza Kaneki <nevin.kadlec@gmail.com>
2024-11-26 22:44:43 -0600

    updated home.html for blog home, and changed views.py to return a render vs the HttpResponse

--- django_blog_project/blog/views.py
+++ django_blog_project/blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+# Create your views here.
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
