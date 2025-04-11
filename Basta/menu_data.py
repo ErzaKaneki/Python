c12452a7c7f6441d9793bdf307c010214e539df7
Erza Kaneki <nevin.kadlec@gmail.com>
2025-04-11 08:45:35 -0500

    Added early bird dinner object  to resaurant, and item diction to menu_data then imported to resaurant.

--- Basta/menu_data.py
+++ Basta/menu_data.py
@@ -9,3 +9,13 @@ brunch_items = {
     'mimosa': 10.50,
     'orange juice': 3.50
 }
+
+early_bird_items ={
+    'salumeria plate': 8.00,
+    'salad and breadsticks (serves 2, no refills)': 14.00,
+    'pizza with quattro formaggi': 9.00,
+    'duck ragu': 17.50,
+    'mushroom ravioli (vegan)': 13.50,
+    'coffee': 1.50,
+    'espresso': 3.00
+}
--- Basta/restaurant.py
+++ Basta/restaurant.py
@@ -1,4 +1,4 @@
-from menu_data import brunch_items
+from menu_data import brunch_items, early_bird_items
 
 class Menu:
     def __init__(self, name, items, start_time, end_time):
@@ -7,4 +7,5 @@ class Menu:
         self.start_time = start_time
         self.end_time = end_time
         
-brunch = Menu("Brunch",)
+brunch = Menu("Brunch", brunch_items, 1100, 1600)
+early_bird_dinners = Menu("Early Bird", early_bird_items, 1500, 1800)
