79f4ba6ab9c30c4cc5e21e78fa26cb04068a8aaf
Erza Kaneki <nevin.kadlec@gmail.com>
2025-04-11 09:19:05 -0500

    Added kids_items to menu_data, added calculate_bill to Menu class, and tested the new Function.

--- Basta/menu_data.py
+++ Basta/menu_data.py
@@ -10,7 +10,7 @@ brunch_items = {
     'orange juice': 3.50
 }
 
-early_bird_items ={
+early_bird_items = {
     'salumeria plate': 8.00,
     'salad and breadsticks (serves 2, no refills)': 14.00,
     'pizza with quattro formaggi': 9.00,
@@ -18,4 +18,20 @@ early_bird_items ={
     'mushroom ravioli (vegan)': 13.50,
     'coffee': 1.50,
     'espresso': 3.00
+}
+
+dinner_items = {
+  'crostini with eggplant caponata': 13.00,
+  'caesar salad': 16.00,
+  'pizza with quattro formaggi': 11.00,
+  'duck ragu': 19.50,
+  'mushroom ravioli (vegan)': 13.50,
+  'coffee': 2.00,
+  'espresso': 3.00
+}
+
+kids_items = {
+    'chicken nuggets': 6.50,
+    'fusilli with wild mushrooms': 12.00,
+    'apple juice': 3.00
 }
--- Basta/restaurant.py
+++ Basta/restaurant.py
@@ -1,11 +1,26 @@
-from menu_data import brunch_items, early_bird_items
+from menu_data import brunch_items, early_bird_items, dinner_items, kids_items
 
 class Menu:
+    
     def __init__(self, name, items, start_time, end_time):
         self.name = name
         self.items = items
         self.start_time = start_time
         self.end_time = end_time
+    
+    def __repr__(self):
+        return f"{self.name} menu available from {self.start_time} to {self.end_time}."
+    
+    def calculate_bill(self, purchased_items):
+        total = 0
+        for items in purchased_items:
+            if items in self.items:
+                total += self.items[items]
+        return f"${total:.2f}"
         
-brunch = Menu("Brunch", brunch_items, 1100, 1600)
-early_bird_dinners = Menu("Early Bird", early_bird_items, 1500, 1800)
+brunch = Menu("Brunch", brunch_items, "11am", "4pm")
+early_bird_dinners = Menu("Early Bird", early_bird_items, "3pm", "6pm")
+dinner = Menu("Dinner", dinner_items, "11am", "9pm")
+kids = Menu("Kids", kids_items, "11am", "9pm")
+
+print(Menu.calculate_bill(brunch, ["pancakes", "home fries", "coffee"]))
