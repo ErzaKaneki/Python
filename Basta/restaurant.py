65fd003ad8f7d5bd3aa1ebd9be8c5d1b30934fc8
Erza Kaneki <nevin.kadlec@gmail.com>
2025-04-11 08:38:45 -0500

    created seperate file for item dictionaries, imported to restaurant.py and added brunch_items to menu_data.py.

---
+++ Basta/restaurant.py
@@ -0,0 +1,10 @@
+from menu_data import brunch_items
+
+class Menu:
+    def __init__(self, name, items, start_time, end_time):
+        self.name = name
+        self.items = items
+        self.start_time = start_time
+        self.end_time = end_time
+        
+brunch = Menu("Brunch",)
