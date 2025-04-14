c12d69a1da36957d8d167e6cac23c5e5991ed75f
Erza Kaneki <nevin.kadlec@gmail.com>
2025-04-14 08:31:52 -0500

    changed tables to dictionaries with the table number as the key

--- Basta/assign_tables.py
+++ Basta/assign_tables.py
@@ -1,21 +1,22 @@
 tables = {
-    1: ['Jiho', False],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: [],
-    7: [],
+    1: {
+        'name': 'Jiho',
+        'vip_status': False,
+        'order': 'Orange Juice, Apple Juice'
+        },
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
 }
+
+print(tables)
 
 def assign_table(table_number, name, vip_status=False):
     if table_number in tables:
-        tables[table_number] = [name, vip_status]
+        tables[table_number]['name'] = name]
+        tables[table_number]['vip_status'] = vip_status
+        tables[table_number]['order'] = ''
 
-assign_table(6, 'Yoni', False)
-
-assign_table(name='Martha', table_number=3, vip_status=True)
-
-assign_table(4, 'Karla')
-
-print(tables)
